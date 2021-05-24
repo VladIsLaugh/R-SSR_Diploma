b00b2cb3840319b905eae2fe0a56062a7c5ee22e
Vladyslav Vit <vladvit19@gmail.com>
2021-05-24 23:59:04 +0300

    to deploy

--- pages/table/index.js
+++ pages/table/index.js
@@ -6,11 +6,15 @@ import MultiSelect from "react-multi-select-component";
 export default function BasicSortingGrid() {
   const [allData, setData] = useState();
   const [selected, setSelected] = useState([]);
-  const [dataGlobal, setgGlobalData] = useState();
+  const [dataGlobal, setgGlobalData] = useState()
 
   const [headers, setheaders] = useState([]);
 
+
+
   // generateTable()
+
+
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -18,48 +22,42 @@ export default function BasicSortingGrid() {
         .then((response) => response.json())
         .then((data1) => {
           //   console.log(data1);
-          setgGlobalData(data1);
+          setgGlobalData(data1)
           let temphead = Object.keys(data1[0]).map((item) => {
             console.log(item);
             return { label: item, value: item };
           });
 
-          let teph = [
-            { label: "Активних хворих", value: "active" },
-            {
-              label: "Активних хворих на мільйон",
-              value: "activePerOneMillion",
-            },
-            { label: "Хворих", value: "cases" },
-            { label: "Хворих на мільйон", value: "casesPerOneMillion" },
-            { label: "Континент", value: "continent" },
-            { label: "Критично хворих", value: "critical" },
-            {
-              label: "Критично хворих на мільйон",
-              value: "criticalPerOneMillion",
-            },
-            { label: "Смертей", value: "deaths" },
-            { label: "Смертей на мільйон", value: "deathsPerOneMillion" },
-            { label: "Людей на 1 хворого", value: "oneCasePerPeople" },
-            { label: "Смертей на 1 хворого", value: "oneDeathPerPeople" },
-            { label: "Тестувань на 1 хворого", value: "oneTestPerPeople" },
-            { label: "Кількість людей", value: "population" },
-            { label: "Вилікуваних", value: "recovered" },
-            {
-              label: "Вилікуваних на мільйон",
-              value: "recoveredPerOneMillion",
-            },
-            { label: "Тестувань", value: "tests" },
-            { label: "Тестувань на мільйон", value: "testsPerOneMillion" },
-            { label: "Сьогодні хворих", value: "todayCases" },
-            { label: "Сьогодні померлих", value: "todayDeaths" },
-            { label: "Сьогодні вилікуваних", value: "todayRecovered" },
-          ];
-
+let teph = [
+  { label: "Активних хворих", value: "active"},
+  { label: "Активних хворих на мільйон", value: "activePerOneMillion"},
+  { label: "Хворих", value: "cases"},
+  { label: "Хворих на мільйон", value: "casesPerOneMillion"},
+  { label: "Континент", value: "continent"},
+  { label: "Критично хворих", value: "critical"},
+  { label: "Критично хворих на мільйон", value: "criticalPerOneMillion"},
+  { label: "Смертей", value: "deaths"},
+  { label: "Смертей на мільйон", value: "deathsPerOneMillion"},
+  { label: "Людей на 1 хворого", value: "oneCasePerPeople"},
+  { label: "Смертей на 1 хворого", value: "oneDeathPerPeople"},
+  { label: "Тестувань на 1 хворого", value: "oneTestPerPeople"},
+  { label: "Кількість людей", value: "population"},
+  { label: "Вилікуваних", value: "recovered"},
+  { label: "Вилікуваних на мільйон", value: "recoveredPerOneMillion"},
+  { label: "Тестувань", value: "tests"},
+  { label: "Тестувань на мільйон", value: "testsPerOneMillion"},
+  { label: "Сьогодні хворих", value: "todayCases"},
+  { label: "Сьогодні померлих", value: "todayDeaths"},
+  { label: "Сьогодні вилікуваних", value: "todayRecovered"}
+]
+          //  console.log();
+          // data1.forEach((element) => {
+          //   temphead.push({ label: element.country, value: element.country });
+          // });
           setheaders(teph);
           setSelected(teph.slice(0, 3));
 
-          onChangeHandler(teph.slice(0, 3));
+          onChangeHandler(teph.slice(0, 3))
           // generateTable();
         });
     };
@@ -67,67 +65,71 @@ export default function BasicSortingGrid() {
     getCountriesData();
   }, []);
 
-  const onChangeHandler = (param) => {
+  const onChangeHandler = (param)=>{
     console.log(param);
-    const columns = geterateColumns(param);
-
-    const rows = geterateRows(dataGlobal);
+    const columns = geterateColumns(param)
+    
+    const rows = geterateRows(dataGlobal)
     console.log(rows);
-    setSelected(param);
-    setData({ columns, rows });
-  };
+    setSelected(param)
+    setData({columns, rows})
+  }
 
-  const geterateColumns = (param) => {
-    let a = param.map((item) => {
+  const geterateColumns = (param) =>{
+    
+    let a =  param.map(item=>{
       return {
         field: item.value,
         headerName: item.label,
         width: 210,
-      };
-    });
+      }
+    })
     a.unshift({
       field: "commodity",
       headerName: "country",
       width: 180,
-    });
-    return a;
-  };
+    })
+    return a
+  }
 
-  const geterateRows = (param) => {
+  const geterateRows = (param) =>{
     console.log(param);
     return param.map((a) => {
-      return {
-        id: `${Date.now()}-${a.country}-${a.cases}`,
-        commodity: a.country,
-        active: a.active,
-        activePerOneMillion: a.activePerOneMillion,
-        cases: a.cases,
-        casesPerOneMillion: a.casesPerOneMillion,
-        continent: a.continent,
-        critical: a.critical,
-        criticalPerOneMillion: a.criticalPerOneMillion,
-        deaths: a.deaths,
-        deathsPerOneMillion: a.deathsPerOneMillion,
-        oneCasePerPeople: a.oneCasePerPeople,
-        oneDeathPerPeople: a.oneDeathPerPeople,
-        oneTestPerPeople: a.oneTestPerPeople,
-        population: a.population,
-        recovered: a.recovered,
-        recoveredPerOneMillion: a.recoveredPerOneMillion,
-        tests: a.tests,
-        testsPerOneMillion: a.testsPerOneMillion,
-        todayCases: a.todayCases,
-        todayDeaths: a.todayDeaths,
-        todayRecovered: a.todayRecovered,
-        updated: a.updated,
-      };
-    });
-  };
+        return {
+          id: `${Date.now()}-${a.country}-${a.cases}`,
+          commodity: a.country,
+          active: a.active,
+          activePerOneMillion: a.activePerOneMillion,
+          cases: a.cases,
+          casesPerOneMillion: a.casesPerOneMillion,
+          continent: a.continent,
+          critical: a.critical,
+          criticalPerOneMillion: a.criticalPerOneMillion,
+          deaths: a.deaths,
+          deathsPerOneMillion: a.deathsPerOneMillion,
+          oneCasePerPeople: a.oneCasePerPeople,
+          oneDeathPerPeople: a.oneDeathPerPeople,
+          oneTestPerPeople: a.oneTestPerPeople,
+          population: a.population,
+          recovered: a.recovered,
+          recoveredPerOneMillion: a.recoveredPerOneMillion,
+          tests: a.tests,
+          testsPerOneMillion: a.testsPerOneMillion,
+          todayCases: a.todayCases,
+          todayDeaths: a.todayDeaths,
+          todayRecovered: a.todayRecovered,
+          updated: a.updated
+        };
+      })
+    
+
+
+  }
 
   return (
     <div style={{ height: 600, width: "100%" }}>
       <div>
-        <h1>Select Fruits</h1>
+        <h1>Обрати вибірку по :</h1>
         {headers && headers.length && (
           <MultiSelect
             options={headers}
